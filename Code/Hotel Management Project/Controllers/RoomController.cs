c7cf1f1ac320fcc54ee37c6a8fed45d3d0af4456
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2021-11-29 15:53:55 +0700

    project update

---
+++ Code/Hotel Management Project/Controllers/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using Hotel_Management_Project.Models;
+using Hotel_Management_Project.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace Hotel_Management_Project.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            RoomDAO rooms = new RoomDAO();
+
+            return View(rooms.GetAllRoom());
+        }
+
+        public IActionResult ShowDetails(int RoomID)
+        {
+            var url = HttpContext.Request.GetEncodedUrl();
+            string last = url.Split('/').Last();
+            RoomDAO room = new RoomDAO();
+            RoomModel foundRoom = null;
+            try
+            {
+                 foundRoom = room.GetRoomByID(Convert.ToInt32(last));
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View(foundRoom);
+        }
+    }
+}
