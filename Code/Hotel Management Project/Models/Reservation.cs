abf5b6ac7da087daa4c9c6846b9d599b1e2c6a6f
quyen <nguyenquyenboss@gmail.com>
2021-12-04 22:04:09 +0700

    update new version

---
+++ Code/Hotel Management Project/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Reservation
+{
+	public int ID { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime checkinDateTime { get; set; }
+    public DateTime checkoutDateTime { get; set; }
+    public string Room { get; set; }
+    public int NumberOfPeople { get; set; }
+
+    public string checkinDateTime => checkinDateTime.ToString("MM/dd/yyyy");
+    public string checkoutDateTime => checkoutDateTime.ToString("MM/dd/yyyy");
+
+
+}
